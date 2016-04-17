2837aaa27f55ffbc2ca3eb2b709ab3cc331d8f30
anothergbr <anothergbr@users.noreply.github.com>
2016-04-16 23:55:37 -0400

    wat is fully functional and can be launched with "java -jar tools/webapp-runner-8.0.30.2.jar build/libs/*.war"

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ a very simple java web app integrated with appdirect's APIs
 
 ## Requirements
 * Java 8
+
+## Running locally
+* `./gradlew bootRun`
+* hit `http://localhost:8080`
+
+## Building a `war`
+* `./gradlew build`
+* the `war` is in `./build/libs/*.war`
+    * you can run it with an embedded tomcat: `java -jar tools/webapp-runner-8.0.30.2.jar build/libs/*.war`
+    * hit `http://localhost:8080`
--- src/main/java/com/gbr/MainEntryPoint.java
+++ src/main/java/com/gbr/MainEntryPoint.java
@@ -2,9 +2,16 @@ package com.gbr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainEntryPoint {
+public class MainEntryPoint extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainEntryPoint.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MainEntryPoint.class, args);
     }
