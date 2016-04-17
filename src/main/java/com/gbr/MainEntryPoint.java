dc45fec715b8b05bc967f8d99410d85203bcd7ff
anothergbr <anothergbr@users.noreply.github.com>
2016-04-16 22:25:37 -0400

    "Hello world" is returned on /

---
+++ src/main/java/com/gbr/MainEntryPoint.java
@@ -0,0 +1,11 @@
+package com.gbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEntryPoint {
+    public static void main(String[] args) {
+        SpringApplication.run(MainEntryPoint.class, args);
+    }
+}
