6586432ec3c879ca2dc9a099a010bd47ea323006
anothergbr <anothergbr@users.noreply.github.com>
2016-04-18 22:23:48 -0400

    minor varargs warning suppressed.

--- src/test/java/com/gbr/nyan/support/SafeMatchers.java
+++ src/test/java/com/gbr/nyan/support/SafeMatchers.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matchers;
 
 public class SafeMatchers {
     @SafeVarargs
+    @SuppressWarnings("varargs")
     public static <T> org.hamcrest.Matcher<java.lang.Iterable<T>> hasItems(org.hamcrest.Matcher<? super T>... itemMatchers) {
         return Matchers.hasItems(itemMatchers);
     }
