61f0aad1a776e664dee816f47e947a8d0679710b
anothergbr <anothergbr@users.noreply.github.com>
2016-04-24 15:42:11 -0400

    Added simple "is /login alive?" test + refactor.

---
+++ src/test/java/com/gbr/nyan/feature/CanShowLoginPage.java
@@ -0,0 +1,35 @@
+package com.gbr.nyan.feature;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CanShowLoginPage {
+    @Autowired
+    private TestRestTemplate httpClient;
+    private ResponseEntity<String> response;
+
+    @Before
+    public void thisPage() {
+        response = httpClient.getForEntity("/login", String.class);
+    }
+
+    @Test
+    public void returnsOkAndUtf8Html() throws Exception {
+        String contentType = response.getHeaders().getContentType().toString();
+        assertThat(response.getStatusCode(), is(OK));
+        assertThat(contentType, is("text/html;charset=UTF-8"));
+    }
+}
