df9b7655bdfb5b1609aeae290d7396f49231bd70
anothergbr <anothergbr@users.noreply.github.com>
2016-04-25 00:00:59 -0400

    change subscription: initial feature test (need a change sample to move forward)

--- src/test/java/com/gbr/nyan/feature/CanChangeSubscription.java
+++ src/test/java/com/gbr/nyan/feature/CanChangeSubscription.java
@@ -1,5 +1,7 @@
 package com.gbr.nyan.feature;
 
+import com.gbr.nyan.domain.Account;
+import com.gbr.nyan.domain.AccountRepository;
 import com.gbr.nyan.domain.User;
 import com.gbr.nyan.domain.UserRepository;
 import com.gbr.nyan.support.FakeAppDirect;
@@ -7,6 +9,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static com.gbr.nyan.domain.Account.Edition.BASIC;
+import static com.gbr.nyan.domain.Account.Edition.PREMIUM;
 import static com.gbr.nyan.support.HttpClientHelper.anAppDirectHttpClient;
 import static com.gbr.nyan.support.HttpClientHelper.get;
 import static com.gbr.nyan.support.Iterables.toList;
@@ -26,7 +30,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class CanCreateSubscription {
+public class CanChangeSubscription {
     @LocalServerPort
     private int serverPort;
     private FakeAppDirect fakeAppDirect;
@@ -34,9 +38,14 @@ public class CanCreateSubscription {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Before
     public void setup() throws Exception {
+        accountRepository.deleteAll();
         userRepository.deleteAll();
+
         fakeAppDirect = FakeAppDirect.create(42534).start();
     }
 
@@ -47,58 +56,54 @@ public class CanCreateSubscription {
 
     @Test
     public void respondsToStatelessEventsWithWellFormedErrorResponse() throws Exception {
-        HttpResponse response = anAppDirectHttpClient().execute(get(createSubscription(), "eventUrl", "http://localhost:42534/v1/events/dummyOrder"));
+        HttpResponse response = anAppDirectHttpClient().execute(get(changeSubscription(), "eventUrl", "http://localhost:42534/v1/events/dummyChange"));
 
-        assertThat(fakeAppDirect.lastRequestPath(), is("/v1/events/dummyOrder"));
+        assertThat(fakeAppDirect.lastRequestPath(), is("/v1/events/dummyChange"));
         assertThat(response.getStatusLine().getStatusCode(), is(200));
         assertThat(EntityUtils.toString(response.getEntity()), is("{\"errorCode\":\"UNKNOWN_ERROR\",\"success\":\"false\"}"));
     }
 
     @Test
-    public void addsUserAndAccountOnCreationEvent() throws Exception {
-        HttpResponse response = anAppDirectHttpClient().execute(get(createSubscription(), "eventUrl", "http://localhost:42534/v1/events/dev-order"));
-        assertThat(fakeAppDirect.lastRequestPath(), is("/v1/events/dev-order"));
+    @Ignore("until I get a sample subscription change!")
+    public void updatesUserEditionAccordingToChangeEvent() throws Exception {
+        Account existingBasicAccount = saveAnAccount(BASIC);
+        saveAUserWith("gabriel.x@gmail.com", existingBasicAccount);
+
+        HttpResponse response = anAppDirectHttpClient().execute(get(changeSubscription(), "eventUrl", "http://localhost:42534/v1/events/dev-change"));
+        assertThat(fakeAppDirect.lastRequestPath(), is("/v1/events/dev-change"));
         assertThat(response.getStatusLine().getStatusCode(), is(200));
 
         List<User> users = toList(userRepository.findAll());
         assertThat(users.size(), is(1));
 
-        User createdUser = users.get(0);
-        assertThat(createdUser.getAccount().getEdition(), is(BASIC));
-        assertThat(createdUser.getFullName(), is("Gabriel SomeLastName"));
-        assertThat(createdUser.getUuid(), is("dac5f67b-7da7-4603-bb1b-3fd507509081"));
-        assertThat(createdUser.getEmail(), is("gabriel.x@gmail.com"));
-        assertThat(createdUser.getOpenIdUrl(), is("https://gabrielspub-test.byappdirect.com/openid/id/dac5f67b-7da7-4603-bb1b-3fd507509081"));
+        User premiumUser = users.get(0);
+        assertThat(premiumUser.getAccount().getEdition(), is(PREMIUM));
+        assertThat(premiumUser.getEmail(), is("gabriel.x@gmail.com"));
 
-        assertThat(EntityUtils.toString(response.getEntity()), is("{\"accountIdentifier\":\"" + createdUser.getAccount().getId() + "\",\"success\":\"true\"}"));
+        assertThat(EntityUtils.toString(response.getEntity()), is("{\"success\":\"true\"}"));
     }
 
-    @Test
-    public void updatesAnExistingUserOnCreationEvent() throws Exception {
-        saveAUserWith("gabriel.x@gmail.com");
+    private Account saveAnAccount(Account.Edition edition) {
+        Account account = new Account();
+        account.setEdition(edition);
 
-        HttpResponse response = anAppDirectHttpClient().execute(get(createSubscription(), "eventUrl", "http://localhost:42534/v1/events/dev-order"));
-        assertThat(response.getStatusLine().getStatusCode(), is(200));
+        accountRepository.save(account);
 
-        List<User> users = toList(userRepository.findAll());
-        assertThat(users.size(), is(1));
-
-        User updatedUser = userRepository.findByEmail("gabriel.x@gmail.com");
-        assertThat(updatedUser.getAccount().getEdition(), is(BASIC));
-        assertThat(updatedUser.getUuid(), is("dac5f67b-7da7-4603-bb1b-3fd507509081"));
+        return account;
     }
 
-    private User saveAUserWith(String email) {
+    private User saveAUserWith(String email, Account account) {
         User user = new User();
         user.setEmail(email);
+        user.setAccount(account);
 
         userRepository.save(user);
 
         return user;
     }
 
-    private String createSubscription() {
-        return baseServerUrl() + "/subscription/create/notification";
+    private String changeSubscription() {
+        return baseServerUrl() + "/subscription/change/notification";
     }
 
     private String baseServerUrl() {
