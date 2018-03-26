f27a3ac42d3fc84ea3ca8ac8de2caeb29d64112b
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2018-03-25 22:04:42 -0400

    Bump to spring-boot 1.5.10 and Java 9.

--- src/main/java/com/gbr/nyan/config/BeansConfig.java
+++ src/main/java/com/gbr/nyan/config/BeansConfig.java
@@ -4,7 +4,7 @@ import com.gbr.nyan.support.HandlebarsRenderer;
 import com.gbr.nyan.web.support.FullRequestLogFilter;
 import com.gbr.nyan.web.support.OauthHttpClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/test/java/com/gbr/nyan/feature/CanCancelSubscription.java
+++ src/test/java/com/gbr/nyan/feature/CanCancelSubscription.java
@@ -12,7 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.web.LocalServerPort;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
